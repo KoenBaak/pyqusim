4b037cbd900fee3f59ab2426db7c98a8aded778c
Koen Baak <koen.baak@mn.nl>
2023-01-08 22:12:16 +0100

    make measure more generic

--- quantalpy/measure.py
+++ quantalpy/measure.py
@@ -1,3 +1,5 @@
+import typing as t
+
 from quantalpy.qpu import QPU
 from quantalpy.runnable import Runnable
 import quantalpy.typing as qpt
@@ -6,17 +8,21 @@ from quantalpy.utils import export
 
 @export
 class Measure(Runnable):
-    def __init__(self, index: int | slice) -> None:
+    def __init__(self, index: int | slice | t.Iterable[int]) -> None:
         self.index = index
 
     @property
     def ends_with_measure(self) -> bool:
         return True
 
-    def run(self, qpu: QPU) -> qpt.MeasureOutcome | None:
-        if isinstance(self.index, int):
-            return qpu.measure(index=self.index)
-        else:
-            return tuple(
-                qpu.measure(index=i) for i in range(*self.index.indices(qpu.n_qubits))
-            )
+    def run(self, qpu: QPU) -> qpt.MeasureOutcome:
+        match self.index:
+            case int():
+                return qpu.measure(index=self.index)
+            case slice():
+                return tuple(
+                    qpu.measure(index=i)
+                    for i in range(*self.index.indices(qpu.n_qubits))
+                )
+            case _:
+                return tuple(qpu.measure(i) for i in self.index)
